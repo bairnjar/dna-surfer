530c7a21373bcff0ef27727eeb5931a62eb33f47
Benjamin Kalman <btkalman@gmail.com>
2019-07-13 11:08:09 -0700

    Collectible stub

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player") {
+            var player = PlayerController.i;
+            player.Collect(this);
+            GameObject.Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,42 @@
+﻿using Btkalman.Unity.Util;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour {
+    public static CollectibleManager i;
+
+    [SerializeField] private Collectible m_collectiblePrefab;
+    [SerializeField] private float m_spawnPeriod = 1f;
+    [SerializeField] private float m_spawnRadius = 1f;
+
+    private float m_lastSpawnTime;
+
+    public void Spawn(Vector2 position) {
+        var collectible = GameObject.Instantiate(m_collectiblePrefab, transform);
+        collectible.transform.position = position;
+    }
+
+    private void Awake() {
+        Singleton.Awake(this, ref i);
+    }
+
+    private void Start() {
+        m_lastSpawnTime = Time.time;
+    }
+
+    private void Update() {
+        UpdateSpawn();
+    }
+
+    private void UpdateSpawn() {
+        float time = Time.time;
+        if (time - m_lastSpawnTime < m_spawnPeriod) {
+            return;
+        }
+        m_lastSpawnTime = time;
+        var player = PlayerController.i;
+        var rot = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+        Spawn((rot * Vector2.up) * m_spawnRadius + player.transform.position);
+    }
+}
