3690f79172d7327f33b959fb89cf27cb7c935c98
Jeremy House <jeremywhouse@gmail.com>
2019-07-14 00:51:17 -0700

    made it so bar resets on death, fixed boost bar display issue

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -95,6 +95,7 @@ public class PlayerController : MonoBehaviour {
 
     private void Reset(bool start = false) {
         currentHealth = m_maxHealth;
+        addToCoinBar(-1000);
         m_rb.rotation = 0f;
         m_rb.velocity = Vector2.zero;
         transform.position = start
